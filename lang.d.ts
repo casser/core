f27c2153ff8ecf09e7dce8c5d91ea1b9ed601853
Sergey Mamyan <sergey.mamyan@gmail.com>
2017-04-14 18:20:34 +0400

    Adding timers

--- lang.d.ts
+++ lang.d.ts
@@ -2,3 +2,10 @@ type Constructor<T> = new(...args: any[]) => T;
 type MapLike<T> = {
     [s:string]:T;
 }
+
+declare function setTimeout(callback: (...args: any[]) => void, ms: number, ...args: any[]): any;
+declare function clearTimeout(timeoutId: any): void;
+declare function setInterval(callback: (...args: any[]) => void, ms: number, ...args: any[]): any;
+declare function clearInterval(intervalId: any): void;
+declare function setImmediate(callback: (...args: any[]) => void, ...args: any[]): any;
+declare function clearImmediate(immediateId: any): void;
